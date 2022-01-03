ccfdcebb58cd655a87fc2919097402eee7028ba9
Gustavo Azevedo <gustavosazevedo@gmail.com>
2022-01-03 16:31:59 -0300

    status loading

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,7 +1,13 @@
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
 import { Overlay } from './styles';
 
-export default function Loader() {
+export default function Loader({ isLoading }) {
+  if (!isLoading) {
+    return null;
+  }
+
   return (
     ReactDOM.createPortal(
       <Overlay>
@@ -11,3 +17,7 @@ export default function Loader() {
     )
   );
 }
+
+Loader.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+};
